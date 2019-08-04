4cd5dfd74b06898970846dab2fd684917ad85cd8
mash9738 <40376636+mash9738@users.noreply.github.com>
2019-08-04 11:39:07 +0530

    aug5

---
+++ inputfunction.py
@@ -0,0 +1,11 @@
+x = input("Enter 1st the number \n")
+print(x)
+y = input("Enter 2nd number \n")
+print(y)
+z = x + y
+print("Sum of a+b= ", z)  # string sum
+
+
+w = int(x) + int(y)
+
+print("Sum of a+b= ", w)
