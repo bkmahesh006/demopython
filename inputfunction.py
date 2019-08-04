a528717c2507f18038908bd315a3519b52458501
mash9738 <40376636+mash9738@users.noreply.github.com>
2019-08-04 12:13:55 +0530

    different inputs

--- inputfunction.py
+++ inputfunction.py
@@ -7,5 +7,20 @@ print("Sum of a+b= ", z)  # string sum
 
 
 w = int(x) + int(y)
+u = float(x) + float(y)
 
 print("Sum of a+b= ", w) # integer sum
+print("Sum of a+b= ", u) # float sum
+
+ch = input("\nEnter character\n") # character input
+print(ch[0])
+ch1 = input("Enter char\n")[0]  # character input
+print(ch1)
+
+
+result = eval(input('Enter an Expression \n')) # expression input
+print(result)
+
+
+
+
